aa23aea28f60c01f342ff48b3ba7503e152e06fa
Carlton Lindsay <carlton.l@outlook.com>
2022-04-05 12:41:48 -0700

    Update contact.js

--- studio/schemas/contact.js
+++ studio/schemas/contact.js
@@ -1,5 +1,3 @@
-import { MdLink } from "react-icons/md";
-
 export default {
   name: "contact",
   title: "Contact Info",
@@ -24,21 +22,7 @@ export default {
         {
           name: "link",
           title: "Social Link",
-          type: "object",
-          icon: MdLink,
-          fields: [
-            {
-              name: "name",
-              title: "Name",
-              description: "(e.g. Facebook)",
-              type: "string",
-            },
-            {
-              name: "url",
-              title: "URL",
-              type: "url",
-            },
-          ],
+          type: "sociallink",
         },
       ],
     },
