55409580321cb77fdc899214146489960a1dbe62
Carlton Lindsay <carlton.l@outlook.com>
2022-04-05 12:41:52 -0700

    Create menuItem.js

---
+++ studio/schemas/objects/menuItem.js
@@ -0,0 +1,55 @@
+import { MdRestaurantMenu } from "react-icons/md";
+
+export default {
+  name: "menuitem",
+  title: "Menu Item",
+  type: "object",
+  icon: MdRestaurantMenu,
+  preview: {
+    select: {
+      title: "itemname",
+      subtitle: "itemprice",
+      // NOTE: Would like to also add number of sub-items to preview
+    },
+  },
+  fields: [
+    {
+      name: "itemname",
+      title: "Item Name",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "itemprice",
+      title: "Item Price",
+      type: "string",
+      description: "(optional) - make sure you include the dollar sign $",
+    },
+    {
+      name: "itemdescription",
+      title: "Item Description",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+    {
+      name: "itemnote",
+      title: "Note",
+      type: "string",
+      description: "(optional) - Will appear as a footer on the menu section",
+    },
+    {
+      name: "subitemlist",
+      title: "Sub-items (optional)",
+      type: "array",
+      description:
+        "Different styles of this item (e.g. Plain Cheese Quesadilla)",
+      of: [
+        {
+          name: "subitem",
+          title: "Sub-item",
+          type: "menusubitem",
+        },
+      ],
+    },
+  ],
+};
