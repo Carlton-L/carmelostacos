0e2251702bf5e30af0da3d5869321b9314a79f9c
Carlton Lindsay <carlton.l@outlook.com>
2022-04-06 13:36:25 -0700

    Update mainLayout.js

--- src/layout/mainLayout.js
+++ src/layout/mainLayout.js
@@ -1,1 +1,24 @@
+import * as React from "react";
+
+import Seo from "../components/seo.js";
+
+import "@fontsource/nanum-pen-script";
 import "../css/typography.css";
+
+const MainLayout = ({
+  children,
+  title = false,
+  description = false,
+  image = false,
+  path = false,
+  props,
+}) => {
+  return (
+    <>
+      <Seo title={title} description={description} image={image} path={path} />
+      {children}
+    </>
+  );
+};
+
+export default MainLayout;
