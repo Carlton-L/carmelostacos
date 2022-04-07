d5f0959db4a88c436a9711bd9f9147a80f18e896
Carlton Lindsay <carlton.l@outlook.com>
2022-04-06 17:24:27 -0700

    Create divider.js

---
+++ src/components/divider.js
@@ -0,0 +1,57 @@
+/** @jsx jsx */
+import * as React from "react";
+import { jsx } from "theme-ui";
+import Border1 from "../images/svg/border-1.svg";
+import Border2 from "../images/svg/border-2.svg";
+import Border3 from "../images/svg/border-3.svg";
+import Border4 from "../images/svg/border-4.svg";
+import Border5 from "../images/svg/border-5.svg";
+import BorderEnd from "../images/svg/border-end.svg";
+
+const Divider = ({ variant }) => {
+  const styles = {
+    height: "100%",
+    width: "1887px",
+    position: "absolute",
+    left: "-943px",
+    top: "-50%",
+  };
+
+  const Component = () => {
+    switch (variant) {
+      case 1:
+        return <Border1 sx={styles} />;
+      case 2:
+        return <Border2 sx={styles} />;
+      case 3:
+        return <Border3 sx={styles} />;
+      case 4:
+        return <Border4 sx={styles} />;
+      case 5:
+        return <Border5 sx={styles} />;
+      case "end":
+        return <BorderEnd sx={styles} />;
+      default:
+        return <Border1 sx={styles} />;
+    }
+  };
+
+  return (
+    <>
+      <hr sx={{ border: "none", margin: "0" }} />
+      <div
+        sx={{
+          height: "200px",
+          width: "auto",
+          mx: "auto",
+          position: "absolute",
+          left: "50%",
+        }}
+      >
+        {Component()}
+      </div>
+    </>
+  );
+};
+
+export default Divider;
