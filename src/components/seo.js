4ff18053c6eb942dee42e63ee418d1423afa3317
Carlton Lindsay <carlton.l@outlook.com>
2022-04-06 19:33:52 -0700

    Create seo.js

---
+++ src/components/seo.js
@@ -0,0 +1,25 @@
+<meta itemScope itemType="http://schema.org/Restaurant">
+  <meta itemProp="name" content="Carmelo's Tacos" />
+  <meta itemProp="acceptsReservations" content={false} />
+  <meta itemProp="image" content="https://i.imgur.com/GQjw7Ry.png" />
+  {/* TODO: Link to menu url */}
+  <meta itemProp="hasMenu" content="menu url" />
+  <meta itemProp="servesCuisine" content="Mexican" />
+  <meta itemProp="starRating" itemScope itemType="https://schema.org/Rating">
+    <meta itemProp="author" content="Tripadvisor" />
+    <meta itemProp="ratingValue" content="4" />
+    <meta
+      itemProp="ratingExplanation"
+      content="Homemade masa tacos with just a few flavors. The al pastor is amazing as are the asadas (The others are good too)."
+    />
+    <meta
+      itemProp="url"
+      content="https://www.tripadvisor.com/Restaurant_Review-g60878-d18281126-Reviews-Carmelos_Tacos-Seattle_Washington.html"
+    />
+  </meta>
+  <meta itemProp="currenciesAccepted" content="USD" />
+  <meta itemProp="paymentAccepted" content="Cash, Credit Card" />
+  <meta itemProp="priceRange" content="$" />
+  <meta itemProp="founder" content="Carmelo Gaspar" />
+  <meta itemProp="url" content="https://carmelostacos.com" />
+</meta>;
