e112b49026ebd3f919f9aa859d1960ffbdfbfbca
Carlton Lindsay <carlton.l@outlook.com>
2022-04-22 10:12:29 -0700

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,25 +1,45 @@
-{/* <meta itemScope itemType="http://schema.org/Restaurant">
-  <meta itemProp="name" content="Carmelo's Tacos" />
-  <meta itemProp="acceptsReservations" content={false} />
-  <meta itemProp="image" content="https://i.imgur.com/GQjw7Ry.png" />
-  {/* TODO: Link to menu url */}
-  <meta itemProp="hasMenu" content="menu url" />
-  <meta itemProp="servesCuisine" content="Mexican" />
-  <meta itemProp="starRating" itemScope itemType="https://schema.org/Rating">
-    <meta itemProp="author" content="Tripadvisor" />
-    <meta itemProp="ratingValue" content="4" />
-    <meta
-      itemProp="ratingExplanation"
-      content="Homemade masa tacos with just a few flavors. The al pastor is amazing as are the asadas (The others are good too)."
-    />
-    <meta
-      itemProp="url"
-      content="https://www.tripadvisor.com/Restaurant_Review-g60878-d18281126-Reviews-Carmelos_Tacos-Seattle_Washington.html"
-    />
-  </meta>
-  <meta itemProp="currenciesAccepted" content="USD" />
-  <meta itemProp="paymentAccepted" content="Cash, Credit Card" />
-  <meta itemProp="priceRange" content="$" />
-  <meta itemProp="founder" content="Carmelo Gaspar" />
-  <meta itemProp="url" content="https://carmelostacos.com" />
-</meta>; */}
+import * as React from "react";
+import Helmet from "react-helmet";
+
+const Seo = () => {
+  return (
+    <Helmet>
+      <meta itemScope itemType="http://schema.org/Restaurant">
+        <meta itemProp="name" content="Carmelo's Tacos" />
+        <meta itemProp="acceptsReservations" content={false} />
+        <meta itemProp="image" content="https://i.imgur.com/GQjw7Ry.png" />
+        {/* TODO: Link to menu url */}
+        <meta itemProp="hasMenu" content="menu url" />
+        <meta itemProp="servesCuisine" content="Mexican" />
+        <meta
+          itemProp="starRating"
+          itemScope
+          itemType="https://schema.org/Rating"
+        >
+          <meta itemProp="author" content="Tripadvisor" />
+          <meta itemProp="ratingValue" content="4" />
+          <meta
+            itemProp="ratingExplanation"
+            content="Homemade masa tacos with just a few flavors. The al pastor is amazing as are the asadas (The others are good too)."
+          />
+          <meta
+            itemProp="url"
+            content="https://www.tripadvisor.com/Restaurant_Review-g60878-d18281126-Reviews-Carmelos_Tacos-Seattle_Washington.html"
+          />
+        </meta>
+        <meta itemProp="currenciesAccepted" content="USD" />
+        <meta itemProp="paymentAccepted" content="Cash, Credit Card" />
+        <meta itemProp="priceRange" content="$" />
+        <meta itemProp="founder" content="Carmelo Gaspar" />
+        <meta itemProp="url" content="https://carmelostacos.com" />
+      </meta>
+      <meta name="format-detection" content="telephone=no"></meta>
+    </Helmet>
+  );
+};
+
+export default Seo;
+
+{
+  /*  */
+}
