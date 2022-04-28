4f17c90d2919f91c57725b87e800c08b3ede9873
Carlton Lindsay <carlton.l@outlook.com>
2022-04-27 19:16:32 -0700

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,9 +1,50 @@
 import * as React from "react";
 import Helmet from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
 
 const Seo = () => {
+  const data = useStaticQuery(graphql`
+    query GetSiteMetadata {
+      site {
+        siteMetadata {
+          description
+          image
+          siteUrl
+          title
+        }
+      }
+    }
+  `);
+
+  const defaults = data?.site?.siteMetadata;
+
+  const title = props.title || defaults.title;
+  const description = props.description || defaults.description;
+  const image = new URL(
+    props.image ||
+      "/carlton-dev/image/upload/v1634593368/Shuffle-for-Spotify/ShuffleForSpotify-og-01_rpa89w.jpg",
+    defaults.image,
+    defaults.siteUrl
+  );
+  const url = new URL(props.path || "/shuffleforspotify/", defaults.siteUrl);
+
   return (
     <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link rel="canonical" href={url} />
+      {image && <meta name="image" content={image} />}
+
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      {image && <meta property="og:image" content={image} />}
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      {image && <meta name="twitter:image" content={image} />}
       <meta itemScope itemType="http://schema.org/Restaurant">
         <meta itemProp="name" content="Carmelo's Tacos" />
         <meta itemProp="acceptsReservations" content={false} />
