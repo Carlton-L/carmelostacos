8290524dcfc84ca08d5bfbf34bf8ba09a0c4c107
Carlton Lindsay <carlton.l@outlook.com>
2022-04-27 19:16:53 -0700

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import Helmet from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
 
-const Seo = () => {
+const Seo = (props) => {
   const data = useStaticQuery(graphql`
     query GetSiteMetadata {
       site {
